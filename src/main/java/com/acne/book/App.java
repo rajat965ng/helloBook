a0c613badeb09640f3f0dc16bb79644165428567
rajat965ng <rajatnigam89@gmail.com>
2017-03-02 08:29:49 +0530

    initial commit

---
+++ src/main/java/com/acne/book/App.java
@@ -0,0 +1,29 @@
+package com.acne.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableAutoConfiguration
+public class App extends SpringBootServletInitializer
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(
+    		SpringApplicationBuilder builder) {
+    	return builder.sources(App.class);
+    }
+    
+}
